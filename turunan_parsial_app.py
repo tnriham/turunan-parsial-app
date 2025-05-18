a146e745b1ec82dc18e4f070827b5e3aee97e809
tnriham <ham_kukembaran@yahoo.com>
2025-05-18 16:33:43 +0700

    Update turunan_parsial_app.py

--- turunan_parsial_app.py
+++ turunan_parsial_app.py
@@ -11,7 +11,7 @@ fungsi_str = st.text_input("😊Masukkan fungsi f(x, y):", "x**2 * y + y**3")
 try:
   f = sp.sympify(fungsi_str)
   fx = sp.diff(f, x)
-  fy = sp.diff(f, y)
+  fy = sp.diff(f, x)
 
   st.latex(f"f(x, y) = {sp.latex(f)}")
   st.latex(f"\\frac{{\\partial f}}{{\\partial x}} = {sp.latex(fx)}")
